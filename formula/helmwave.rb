e2f0a2de599790729fc64bf53b68fd601490abd8
goreleaserbot <goreleaser@carlosbecker.com>
2021-07-28 10:29:47 +0300

    Brew formula update for helmwave version v0.12.6

--- formula/helmwave.rb
+++ formula/helmwave.rb
@@ -5,29 +5,29 @@
 class Helmwave < Formula
   desc "HelmWave is like docker-compose for helm"
   homepage ""
-  version "0.12.5"
+  version "0.12.6"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/helmwave/helmwave/releases/download/v0.12.5/helmwave_0.12.5_darwin_amd64.tar.gz"
-      sha256 "6815c6d82faf7603798b34ea1e308b326904ddd9d90aa76007a322547fbecb39"
+      url "https://github.com/helmwave/helmwave/releases/download/v0.12.6/helmwave_0.12.6_darwin_amd64.tar.gz"
+      sha256 "a0c19fb39b91454e4c02706af4d4c07af834f466c18dc5028d90dabec761c9bb"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/helmwave/helmwave/releases/download/v0.12.5/helmwave_0.12.5_darwin_arm64.tar.gz"
-      sha256 "76a2d59db2089097a777c31dd870688c7f58efa00d3e6ab795774d8ccd7681d4"
+      url "https://github.com/helmwave/helmwave/releases/download/v0.12.6/helmwave_0.12.6_darwin_arm64.tar.gz"
+      sha256 "27069183ee7f455d6c0595ff13fa1fff7b82be9dc1458e35c951c0f64588b31d"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/helmwave/helmwave/releases/download/v0.12.5/helmwave_0.12.5_linux_amd64.tar.gz"
-      sha256 "e5281711b1a31880aeae3a836ad3f835b7791059f3f4ce5327ba259696b26abc"
+      url "https://github.com/helmwave/helmwave/releases/download/v0.12.6/helmwave_0.12.6_linux_amd64.tar.gz"
+      sha256 "bdef8ac2fa4195b3d03322b8b799553c7b2d0e42cf89ed4ddb71522a9b853795"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/helmwave/helmwave/releases/download/v0.12.5/helmwave_0.12.5_linux_arm64.tar.gz"
-      sha256 "0db2a2edbe6f866d186fe5b6439c3b96e0c3ea8ddcd3bcbda8dc1897b2b7192b"
+      url "https://github.com/helmwave/helmwave/releases/download/v0.12.6/helmwave_0.12.6_linux_arm64.tar.gz"
+      sha256 "d62093456480bf96e5e536906e291b643288f9f1ab7cbcd830a4d92960698d8f"
     end
   end
 
