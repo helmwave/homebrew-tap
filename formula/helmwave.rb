4156f83606888b4259f3018c68672aec45e10eaf
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-08 13:16:33 +0300

    Brew formula update for helmwave version v0.17.3

--- formula/helmwave.rb
+++ formula/helmwave.rb
@@ -5,21 +5,21 @@
 class Helmwave < Formula
   desc "HelmWave is like docker-compose for helm"
   homepage ""
-  version "0.17.2"
+  version "0.17.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/helmwave/helmwave/releases/download/v0.17.2/helmwave_0.17.2_darwin_amd64.tar.gz"
-      sha256 "7ff2a6777e3125761d010aa8f28829ceb88fb923822c6c232470c97edfd04358"
+      url "https://github.com/helmwave/helmwave/releases/download/v0.17.3/helmwave_0.17.3_darwin_amd64.tar.gz"
+      sha256 "65c1931074ad5654a2d9b1ead6b0b81669a765fdfe23ead84138d0e9321c7ed0"
 
       def install
         bin.install "helmwave"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/helmwave/helmwave/releases/download/v0.17.2/helmwave_0.17.2_darwin_arm64.tar.gz"
-      sha256 "7ed3611e944708bf7685e922de5a5cca4ff3b97ea4934abd5a56db482d957dae"
+      url "https://github.com/helmwave/helmwave/releases/download/v0.17.3/helmwave_0.17.3_darwin_arm64.tar.gz"
+      sha256 "9c5a2a0204e02e7745bca72029ee20482e9b68c68f687cc52ee31a0c69e5aa4d"
 
       def install
         bin.install "helmwave"
@@ -29,16 +29,16 @@ class Helmwave < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/helmwave/helmwave/releases/download/v0.17.2/helmwave_0.17.2_linux_arm64.tar.gz"
-      sha256 "cd59709e91332ce4ac879df5952a0d4c8cbc537244ec0d88e3d2d3bc2d5a7e23"
+      url "https://github.com/helmwave/helmwave/releases/download/v0.17.3/helmwave_0.17.3_linux_arm64.tar.gz"
+      sha256 "a59b374a757357aabd0ad9afbd3612dd564d3ebb181c6db0a95d2e1d245fdda5"
 
       def install
         bin.install "helmwave"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/helmwave/helmwave/releases/download/v0.17.2/helmwave_0.17.2_linux_amd64.tar.gz"
-      sha256 "b474e437b25f9d535640f1a63b2bc4ad55ccc1210a79f041262046120f944ac9"
+      url "https://github.com/helmwave/helmwave/releases/download/v0.17.3/helmwave_0.17.3_linux_amd64.tar.gz"
+      sha256 "4450c4a4e3f37d6eeed1f94ea0632c6e9487afc9269919c88bda2263755bbfff"
 
       def install
         bin.install "helmwave"
